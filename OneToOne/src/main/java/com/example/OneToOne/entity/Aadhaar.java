1b91c7810665bec72cf0b94fddd9171003ca27d8
Bannibasava <68640815+Bannibasava@users.noreply.github.com>
2026-01-04 23:09:40 +0530

    Add files via upload

---
+++ OneToOne/src/main/java/com/example/OneToOne/entity/Aadhaar.java
@@ -0,0 +1,25 @@
+package com.example.OneToOne.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Aadhaar {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false,unique = true)
+    private Long aadhaarNumber;
+
+
+
+}
